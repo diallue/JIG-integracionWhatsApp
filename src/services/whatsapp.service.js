8d3c535c232c62c3585eb526d8dcbb0a76ff2037
Diego Allue <dallue@jig.es>
2026-09-09 13:20:43 +0200

    Add enviarMensajeTexto function for sending text messages via WhatsApp

--- src/services/whatsapp.service.js
+++ src/services/whatsapp.service.js
@@ -1,6 +1,37 @@
 const whatsappToken = process.env.WHATSAPP_TOKEN;
 const phoneNumberId = process.env.PHONE_NUMBER_ID;
 
+async function enviarMensajeTexto(destinatario, texto) {
+  const url = `https://graph.facebook.com/v25.0/${process.env.PHONE_NUMBER_ID}/messages`;
+
+  const payload = {
+    messaging_product: "whatsapp",
+    recipient_type: "individual",
+    to: destinatario,
+    type: "text",
+    text: { 
+      preview_url: true,
+      body: texto 
+    }
+  };
+
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${process.env.WHATSAPP_TOKEN}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload)
+    });
+
+    const result = await response.json();
+    console.log("[AUTOMATIZACIÓN ENVIADA]:", result);
+  } catch (error) {
+    console.error("[ERROR HTTP]:", error);
+  }
+}
+
 async function enviarPlantillaHorarios(destinatario) {
   const url = `https://graph.facebook.com/v25.0/${phoneNumberId}/messages`;
   const payload = {
